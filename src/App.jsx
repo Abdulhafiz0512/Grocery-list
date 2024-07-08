20e149cc15a325f87228e16467ee8e8719d2f2dc
Bekzod <bekzodisakov18@gmail.com>
2024-07-08 19:35:21 +0500

    edit base url

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [fetchError, setFetchError] = useState(null);
 
-  const api_url = "http://localhost:3000";
+  const api_url = "https://posts-server-1.onrender.com";
   useEffect(() => {
     async function fetchItems() {
       try {
